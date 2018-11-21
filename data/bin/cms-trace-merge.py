a9b29fe3d1bdda11f53e37456f9e7007d52c32c5
jensenzhang <jingxuan.n.zhang@gmail.com>
2018-11-21 15:23:41 -0500

    Add script for file merge
    Signed-off-by: jensenzhang <jingxuan.n.zhang@gmail.com>

---
+++ data/bin/cms-trace-merge.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import os
+import json
+
+def load_files_replicas(basedir):
+    files = []
+    replicas = []
+    for d in os.listdir(basedir):
+        d = os.path.join(basedir, d)
+        if os.path.isdir(d):
+            files_path = os.path.join(d, 'files.json')
+            with open(files_path) as ff:
+                new_files = json.load(ff)
+                files.extend(new_files)
+
+            replicas_path = os.path.join(d, 'replicas.json')
+            with open(replicas_path) as fr:
+                new_replicas = json.load(fr)
+                replicas.extend(new_replicas)
+    return files, replicas
+
+if __name__ == '__main__':
+    import sys
+    basedir = sys.argv[1]
+    files, replicas = load_files_replicas(basedir)
+
+    files_path = os.path.join(basedir, 'files.json')
+    with open(files_path, 'w') as ff:
+        ff.write(json.dumps(files, indent=4, sort_keys=True))
+
+    replicas_path = os.path.join(basedir, 'replicas.json')
+    with open(replicas_path, 'w') as fr:
+        fr.write(json.dumps(replicas, indent=4, sort_keys=True))
